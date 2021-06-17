1012bd70a56ccfa2c3f70eab714dbcedea09c2a3
Roma <zeromistakes4@gmail.com>
2021-06-17 16:26:13 +0300

    catalogue(desctop) completed

--- src/js/script.js
+++ src/js/script.js
@@ -13,4 +13,24 @@ $(document).ready(function(){
               }
         ]
     });
+
+    $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function() {
+      $(this)
+        .addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+        .closest('div.container').find('div.catalogue__content').removeClass('catalogue__content_active').eq($(this).index()).addClass('catalogue__content_active');
+    });
+
+    function toggleSlide(slide) {
+      $(slide).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+          $('.catalogue-item__list').eq(i).toggleClass('catalogue-item__list_active');
+        })
+      });
+    };
+
+    toggleSlide('.catalogue-item__link');
+    toggleSlide('.catalogue-item__back');
+    
   });
