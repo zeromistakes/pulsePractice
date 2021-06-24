65d2b79215536b682a644868ed5096e1fadc47c6
Roma <zeromistakes4@gmail.com>
2021-06-25 00:58:06 +0300

    modal markup + script

--- src/js/script.js
+++ src/js/script.js
@@ -33,4 +33,18 @@ $(document).ready(function(){
     toggleSlide('.catalogue-item__link');
     toggleSlide('.catalogue-item__back');
     
+    // modal 
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+    $('.button_mini').each(function(i){
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalogue-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn();
+      });
+    });
   });
